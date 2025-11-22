f8fc0a03a4d784ef3971ce5eef411b2598b294ad
Rahul Thapliyal <9.rahul02@gmail.com>
2025-11-22 15:38:17 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,16 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export const metadata = {
   title: "AI Chatbot – Gemini + Next.js",
   description: "A Gemini-powered AI chatbot built with Next.js and MongoDB.",
@@ -16,3 +29,15 @@ export const metadata = {
     ],
   },
 };
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
+
